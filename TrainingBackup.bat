1752e05eb25e418bcd8014b624f5ef07a1d877c5
Ivan Pavlak <114421403+Privlak@users.noreply.github.com>
2024-02-26 14:25:38 +0100

    Delete TrainingBackup.bat

--- TrainingBackup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Users\Ivan\miniconda3\Scripts\activate.bat" "C:\Users\Ivan\miniconda3"
-call conda activate TrainingBackup
-python E:\GitHub\VSCode_Windows\Training_Backup\TrainingBackup.py
