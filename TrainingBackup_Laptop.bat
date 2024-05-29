980e8f4f4ef4612845746ef047779efb4a781dc1
Ivan Pavlak <ivanpavlak00@gmail.com>
2024-05-29 21:31:01 +0200

    Add separate files to allow the script to execute on both PC and laptop

---
+++ TrainingBackup_Laptop.bat
@@ -0,0 +1,5 @@
+@echo off
+call "C:\Users\Ivan\miniconda3\Scripts\activate.bat" "C:\Users\Ivan\miniconda3"
+call conda activate TrainingBackup
+python C:\Users\Ivan\VSCode\GitHub\Training_Backup\TrainingBackup_Laptop.py
+
