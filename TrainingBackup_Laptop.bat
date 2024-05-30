ed670ef273efb5d1ef3b2f5b71bbbd7dc30c6a3a
Ivan Pavlak <ivanpavlak00@gmail.com>
2024-05-31 00:20:00 +0200

    Removed and refactored for use in single files

--- TrainingBackup_Laptop.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "C:\Users\Ivan\miniconda3\Scripts\activate.bat" "C:\Users\Ivan\miniconda3"
-call conda activate TrainingBackup
-python C:\Users\Ivan\VSCode\GitHub\Training_Backup\TrainingBackup_Laptop.py
-
