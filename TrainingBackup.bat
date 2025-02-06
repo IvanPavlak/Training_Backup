de58b3f242b30b5fe1f4021e0227a2c8657a8cbd
IvanPavlak <ivanpavlak00@gmail.com>
2025-02-06 10:08:02 +0100

    Update paths

--- TrainingBackup.bat
+++ TrainingBackup.bat
@@ -2,9 +2,9 @@
 
 :: Check hostname to determine which Python script to run
 if "%COMPUTERNAME%" == "PC-WIN11" ( 
-    set "python_script=E:\VSCode\GitHub\Training_Backup\TrainingBackup.py"
+    set "python_script=E:\Development\GitHub\Training_Backup\TrainingBackup.py"
 ) else (
-    set "python_script=C:\Users\Ivan\VSCode\GitHub\Training_Backup\TrainingBackup.py"
+    set "python_script=C:\Users\Ivan\Development\GitHub\Training_Backup\TrainingBackup.py"
 )
 
 :: Activate the conda environment and run the Python script
--- TrainingBackupManualUpdate.ps1
+++ TrainingBackupManualUpdate.ps1
@@ -1,18 +1,16 @@
-# Training-Backup Function
 function Training-Backup {
 	$currentDirectory = Get-Location
-	$backupDirectory = "E:\VSCode\GitHub\Training_Backup"
+	$backupDirectory = "E:\Development\GitHub\Training_Backup"
     
 	$hostname = (Get-CimInstance Win32_ComputerSystem).Name
 	if ($hostname -eq "Laptop-Win11") {
-		$backupDirectory = "C:\Users\Ivan\VSCode\GitHub\Training_Backup"
+		$backupDirectory = "C:\Users\Ivan\Development\GitHub\Training_Backup"
 	}
     
 	Set-Location -Path $backupDirectory
     
 	try {
 		& ".\TrainingBackup.bat"
-		# Output success message in green
 		Write-Host "`n=> Training Backup Completed!`n" -ForegroundColor Green
 	}
 	catch {
