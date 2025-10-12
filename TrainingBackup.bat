443a8d17e70387b7121215d6d4f1bc719599dac1
IvanPavlak <ivanpavlak00@gmail.com>
2025-10-12 23:16:01 +0200

    Update configuration.json and TrainingBackup.bat

--- TrainingBackup.bat
+++ TrainingBackup.bat
@@ -1,11 +1,6 @@
 @echo off
 
-:: Check hostname to determine which Python script to run
-if "%COMPUTERNAME%" == "PC-WIN11" ( 
-    set "python_script=E:\Development\GitHub\Training_Backup\TrainingBackup.py"
-) else (
-    set "python_script=C:\Users\Ivan\Development\GitHub\Training_Backup\TrainingBackup.py"
-)
+set "python_script=C:\Users\Ivan\Development\GitHub\Training_Backup\TrainingBackup.py"
 
 :: Activate the conda environment and run the Python script
 call "C:\Users\Ivan\miniconda3\Scripts\activate.bat" "C:\Users\Ivan\miniconda3"
